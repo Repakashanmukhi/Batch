bc76dda02f880a1bdbe78c38013547d527de2457
Repaka Shanmukhi <shanmukhirepaka@gmail.com>
2025-01-02 08:17:08 +0000

    Updated

--- webapp/controller/Batch.controller.js
+++ webapp/controller/Batch.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
      "sap/ui/core/mvc/Controller",
      "sap/ui/model/json/JSONModel", 
-     "batchoperations/model/formatter"
-], (Controller,JSONModel,formatter) => {
+     "batchoperations/model/formatter",
+     "sap/m/MessageToast"
+], (Controller,JSONModel,formatter,MessageToast) => {
     "use strict";
      var that;
      var oSelectedPath =[]
@@ -12,12 +13,12 @@ sap.ui.define([
         onInit() 
         {
             that=this;
-    
-            
+            var oModel=that.getOwnerComponent().getModel();
+            that.getView().setModel(oModel)
             var TempEmployee = new JSONModel({
                 Employees: []
             });
-            this.getView().setModel(TempEmployee, "employeeModel");
+            that.getView().setModel(TempEmployee, "employeeModel");
             if(!that.create){
             that.create=sap.ui.xmlfragment("batchoperations.fragments.create",that)
             }
@@ -71,9 +72,9 @@ sap.ui.define([
                         FirstName: oEmployee.FirstName,
                         Email: oEmployee.Email,
                         Phone: oEmployee.Phone,
+                        BloodGroup: oEmployee.BloodGroup,
                         Department: oEmployee.Department,
                         Position: oEmployee.Position,
-                        BloodGroup: oEmployee.BloodGroup,
                         JoiningDate: oEmployee.JoiningDate
                     };
                     oData.create("/EmployeeInfo", oNewEmployee, {
@@ -94,7 +95,68 @@ sap.ui.define([
             oSelectedItems:function(oEvent){
                 oSelectedPath = oEvent.getSource().getSelectedContextPaths()
             },
-            onDelete: function(){
+            onUpdateDialog: function(){
+            if (!that.update) {
+                that.update = sap.ui.xmlfragment("batchoperations.fragments.update", that);
+            }
+            that.update.open();  
+            var oModel = that.getView().getModel("employeeModel");
+            var eStore = oModel.getProperty("/Employees");
+            var oTable = that.getView().byId("employeeTable");
+            var oSelectedItems = oTable.getSelectedItems();
+            var selectedEmployees = [];
+            for (var i = 0; i < oSelectedItems.length; i++) {
+                var item = oSelectedItems[i];
+                var oEmp = item.getBindingContext().getObject();
+                selectedEmployees.push(oEmp); 
+            }
+            var oSelectedEmployeesModel = new sap.ui.model.json.JSONModel();
+            oSelectedEmployeesModel.setData({ selectedEmployees: selectedEmployees });
+            that.update.setModel(oSelectedEmployeesModel, "selectedEmployeesModel");
+        },
+        onSave: function(){
+            var oSelectedEmployeesModel = that.update.getModel("selectedEmployeesModel");
+            var aSelectedEmployees = oSelectedEmployeesModel.getProperty("/selectedEmployees");
+            var oEmployeeModel = this.getView().getModel("employeeModel");
+            var aEmployees = oEmployeeModel.getProperty("/Employees");
+            for (var i = 0; i < aSelectedEmployees.length; i++) {
+                var oSelectedEmployee = aSelectedEmployees[i];
+                var index = aEmployees.findIndex(function (oEmployee) {
+                    return oEmployee.Email === oSelectedEmployee.Email;
+                });
+                if (index !== -1) {
+                    aEmployees[index] = Object.assign({}, aEmployees[index], oSelectedEmployee);
+                }
+            }
+            oEmployeeModel.setProperty("/Employees", aEmployees);
+            // Updating Single row
+            var oData = this.getOwnerComponent().getModel();
+            for (var j = 0; j < aSelectedEmployees.length; j++) {
+                var oEmployee = aSelectedEmployees[j];
+                var oUpdatedEmployee = {
+                    FirstName: oEmployee.FirstName,
+                    Email: oEmployee.Email,
+                    Phone: oEmployee.Phone,
+                    BloodGroup: oEmployee.BloodGroup,
+                    Department: oEmployee.Department,
+                    Position: oEmployee.Position,
+                    JoiningDate: oEmployee.JoiningDate
+                };
+                oData.update("/EmployeeInfo('" + oEmployee.Email + "')", oUpdatedEmployee, {
+                    success: function () {
+                        MessageToast.show("Employee data updated successfully!");
+                    },
+                    error: function () {
+                        MessageToast.show("Error updating employee data!");
+                    }
+                });
+            }
+        
+        },
+        onClose: function(){
+            that.update.close();
+        },
+            onDelete: function(){ 
                 var oTable = this.byId("employeeTable"); 
                 var aSelectedItems = oTable.getSelectedItems()
                var oModel =this.getView().getModel();
@@ -112,15 +174,42 @@ sap.ui.define([
                             deleteNextRecord();  
                         },
                         error: function() {
-                            sap.m.MessageToast.show("Error deleting record at path: " + sPath);
+                            sap.m.MessageToast.show("Error deleting record: " + sPath);
                             deleteNextRecord();  
                         }
-                    });
+                    });         
                 } else {
                     sap.m.MessageToast.show("All selected records deleted successfully!");
                 }
             };
             deleteNextRecord();
-        }
+        },
+        onSearch: function(oEvent){
+            var oItem = oEvent.getParameter("suggestionItem");
+			if (oItem) {
+				MessageToast.show("Search for: " + oItem.getText());
+			} else {
+				MessageToast.show("Search is fired!");
+			}
+		},
+        onSuggest: function(event){
+            var sValue = event.getParameter("suggestValue"),
+				aFilters = [];
+			if (sValue) {
+				aFilters = [
+					new Filter([
+						new Filter("ProductId", function (sText) {
+							return (sText || "").toUpperCase().indexOf(sValue.toUpperCase()) > -1;
+						}),
+						new Filter("Name", function (sDes) {
+							return (sDes || "").toUpperCase().indexOf(sValue.toUpperCase()) > -1;
+						})
+					], false)
+				];
+			}
+
+			this.oSF.getBinding("suggestionItems").filter(aFilters);
+			this.oSF.suggest();        
+        },
     });
 });
